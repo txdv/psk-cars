ed0748d63863e5a08d179a776f7e166f516ee9fc
Andrius Bentkus <andrius.bentkus@mif.stud.vu.lt>
2019-04-23 22:22:53 +0300

    mybatis initial

--- src/main/java/lt/vu/usecases/cdi/simple/RequestUseCaseControllerMyBatis.java
+++ src/main/java/lt/vu/usecases/cdi/simple/RequestUseCaseControllerMyBatis.java
@@ -2,6 +2,8 @@ package lt.vu.usecases.cdi.simple;
 
 import lombok.Getter;
 import lombok.extern.slf4j.Slf4j;
+import lt.vu.usecases.mybatis.dao.*;
+import lt.vu.usecases.mybatis.model.*;
 
 import javax.annotation.PostConstruct;
 import javax.enterprise.inject.Model;
@@ -13,4 +15,38 @@ import java.util.List;
 @Slf4j
 public class RequestUseCaseControllerMyBatis {
 
+    @Getter
+    private Owner owner = new Owner();
+    @Getter
+    private Car car = new Car();
+    @Getter
+    private Insurance insurance = new Insurance();
+
+    @Getter
+    private List<Insurance> allInsurances;
+
+    @PostConstruct
+    public void init() {
+        loadAllInsurances();
+    }
+
+    @Inject
+    private OwnerMapper ownerMapper;
+    @Inject
+    private CarMapper carMapper;
+    @Inject
+    private InsuranceMapper insuranceMapper;
+
+    @Transactional
+    public void createCourseStudent() {
+        ownerMapper.insert(owner);
+        car.setOwnerId(owner.getId());
+        carMapper.insert(car);
+
+        log.info("Maybe OK...");
+    }
+
+    private void loadAllInsurances() {
+        allInsurances = insuranceMapper.selectAll();
+    }
 }
